6fa02cf2b0abee56308074024307d3aab82664af
renventura <rmvohio@gmail.com>
2017-09-23 15:00:22 -0400

    Hide overflow from sections

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -52,14 +52,14 @@ if ( ! function_exists( 'special_entry_footer' ) ) :
 			$categories_list = get_the_category_list( esc_html__( ', ', 'special' ) );
 			if ( $categories_list ) {
 				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'special' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+				printf( '<div class="cat-links">' . esc_html__( 'Posted in %1$s', 'special' ) . '</div>', $categories_list ); // WPCS: XSS OK.
 			}
 
 			/* translators: used between list items, there is a space after the comma */
 			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'special' ) );
 			if ( $tags_list ) {
 				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'special' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+				printf( '<div class="tags-links">' . esc_html__( 'Tagged %1$s', 'special' ) . '</div>', $tags_list ); // WPCS: XSS OK.
 			}
 		}
 
